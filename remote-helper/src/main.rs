f5094c374db0a2e3876d4eb7c7260572466defb3
Alexis Sellier <self@cloudhead.io>
2021-11-04 21:38:30 +0100

    remote-helper: Sketch of project-id support

--- remote-helper/src/main.rs
+++ remote-helper/src/main.rs
@@ -3,7 +3,11 @@ use ethers::contract::abigen;
 use ethers::prelude::*;
 use ethers::types::{Address, NameOrAddress};
 
+use librad::crypto::{BoxedSigner, SomeSigner};
 use link_identities::git::Urn;
+use radicle_git_helpers::remote_helper;
+
+use rad_remote_helper::signer::Signer;
 
 use anyhow::anyhow;
 use anyhow::bail;
@@ -11,6 +15,7 @@ use anyhow::Context as _;
 
 use std::convert::TryFrom;
 use std::env;
+use std::fs::File;
 use std::future;
 use std::process;
 use std::process::Command;
@@ -18,9 +23,9 @@ use std::process::Stdio;
 use std::str::FromStr;
 
 #[derive(Debug, Clone)]
-struct Remote {
-    org: NameOrAddress,
-    urn: Urn,
+enum Remote {
+    Org { org: NameOrAddress, urn: Urn },
+    Project { urn: Urn },
 }
 
 /// Text record key that holds the Git server address.
@@ -42,38 +47,42 @@ impl FromStr for Remote {
     type Err = anyhow::Error;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let url = url::Url::parse(input).unwrap();
+        if let Ok(url) = url::Url::parse(input) {
+            if url.scheme() != URL_SCHEME {
+                bail!("Invalid URL scheme {:?}", url.scheme());
+            }
+            if url.cannot_be_a_base() {
+                bail!("Invalid URL {:?}", input);
+            }
 
-        if url.scheme() != URL_SCHEME {
-            bail!("Invalid URL scheme {:?}", url.scheme());
-        }
-        if url.cannot_be_a_base() {
-            bail!("Invalid URL {:?}", input);
-        }
+            let host = url
+                .host_str()
+                .map(|h| h.trim_end_matches(".git"))
+                .ok_or_else(|| anyhow!("Invalid URL host {:?}", input))?;
+            let path = url
+                .path()
+                .strip_prefix('/')
+                .ok_or_else(|| anyhow!("Missing URL path: {:?}", input))?;
 
-        let host = url
-            .host_str()
-            .map(|h| h.trim_end_matches(".git"))
-            .ok_or_else(|| anyhow!("Invalid URL host {:?}", input))?;
-        let path = url
-            .path()
-            .strip_prefix('/')
-            .ok_or_else(|| anyhow!("Missing URL path: {:?}", input))?;
+            let org = if let Ok(addr) = host.parse::<Address>() {
+                NameOrAddress::Address(addr)
+            } else if host.contains('.') {
+                NameOrAddress::Name(host.to_owned())
+            } else {
+                bail!(
+                    "Invalid URL host {:?}: expected a domain name or Ethereum address",
+                    host
+                );
+            };
+            let urn = Urn::from_str(&format!("rad:git:{}", path))
+                .with_context(|| format!("Invalid project identifier {:?}", path))?;
 
-        let org = if let Ok(addr) = host.parse::<Address>() {
-            NameOrAddress::Address(addr)
-        } else if host.contains('.') {
-            NameOrAddress::Name(host.to_owned())
+            Ok(Self::Org { org, urn })
         } else {
-            bail!(
-                "Invalid URL host {:?}: expected a domain name or Ethereum address",
-                host
-            );
-        };
-        let urn = Urn::from_str(&format!("rad:git:{}", path))
-            .with_context(|| format!("Invalid project identifier {:?}", path))?;
+            let urn = Urn::from_str(&format!("rad:git:{}", input))?;
 
-        Ok(Self { org, urn })
+            Ok(Self::Project { urn })
+        }
     }
 }
 
@@ -92,7 +101,7 @@ async fn main() {
         fatal(anyhow!("Not enough arguments supplied"));
     };
 
-    match url.parse() {
+    match Remote::from_str(&url) {
         Ok(url) => {
             if let Err(err) = run(url).await {
                 fatal(err);
@@ -105,21 +114,41 @@ async fn main() {
 }
 
 async fn run(remote: Remote) -> anyhow::Result<()> {
-    let domain = resolve(remote.org).await?;
-    let http_url = format!("https://{}/{}", domain, remote.urn.encode_id());
+    match remote {
+        Remote::Org { org, urn } => {
+            let domain = resolve(org).await?;
+            let http_url = format!("https://{}/{}", domain, urn.encode_id());
 
-    // TODO: Use `exec` here.
-    let mut child = Command::new("git")
-        .arg("remote-https")
-        .arg(http_url)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .stdin(Stdio::inherit())
-        .spawn()?;
+            // TODO: Use `exec` here.
+            let mut child = Command::new("git")
+                .arg("remote-https")
+                .arg(http_url)
+                .stdout(Stdio::inherit())
+                .stderr(Stdio::inherit())
+                .stdin(Stdio::inherit())
+                .spawn()?;
 
-    let status = child.wait()?;
+            let status = child.wait()?;
 
-    process::exit(status.code().unwrap_or(EXIT_FAILURE))
+            process::exit(status.code().unwrap_or(EXIT_FAILURE))
+        }
+        Remote::Project { urn: _urn } => {
+            let config = if let Ok(identity_path) = env::var("RAD_IDENTITY") {
+                let identity = File::open(&identity_path)
+                    .with_context(|| format!("unable to open {:?}", &identity_path))?;
+                let signer = Signer::new(identity)
+                    .with_context(|| format!("unable to load identity {:?}", &identity_path))?;
+                let boxed_signer = BoxedSigner::from(SomeSigner { signer });
+
+                remote_helper::Config {
+                    signer: Some(boxed_signer),
+                }
+            } else {
+                remote_helper::Config::default()
+            };
+            remote_helper::run(config)
+        }
+    }
 }
 
 async fn resolve(org: NameOrAddress) -> anyhow::Result<String> {
