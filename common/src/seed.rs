16405d4fddbb4bd2dd75ac8db47ae6b9a576ef2a
Alexis Sellier <self@cloudhead.io>
2022-01-20 16:31:38 +0100

    rad-publish: Allow passing `--seed`
    Signed-off-by: Alexis Sellier <self@cloudhead.io>

--- common/src/seed.rs
+++ common/src/seed.rs
@@ -2,22 +2,24 @@ use std::path::Path;
 
 use anyhow::Result;
 use librad::crypto::peer::PeerId;
+use url::Url;
 
 use crate::git;
 
 pub fn push_delegate_id(
     repo: &Path,
-    seed: &str,
+    seed: &Url,
     self_id: &str,
     peer_id: PeerId,
 ) -> Result<String, anyhow::Error> {
+    let url = seed.join(self_id)?;
+
     git::git(
         repo,
         [
             "push",
-            "-q",
             "--signed",
-            &format!("{}/{}", seed, self_id),
+            url.as_str(),
             &format!(
                 "refs/namespaces/{}/refs/rad/id:refs/remotes/{}/rad/id",
                 self_id, peer_id
@@ -28,18 +30,19 @@ pub fn push_delegate_id(
 
 pub fn push_project_id(
     repo: &Path,
-    seed: &str,
+    seed: &Url,
     project_id: &str,
     peer_id: PeerId,
 ) -> Result<String, anyhow::Error> {
+    let url = seed.join(project_id)?;
+
     git::git(
         repo,
         [
             "push",
-            "-q",
             "--signed",
             "--atomic",
-            &format!("{}/{}", seed, project_id),
+            url.as_str(),
             &format!(
                 "refs/namespaces/{}/refs/rad/id:refs/remotes/{}/rad/id",
                 project_id, peer_id
@@ -50,18 +53,19 @@ pub fn push_project_id(
 
 pub fn push_refs(
     repo: &Path,
-    seed: &str,
+    seed: &Url,
     project_id: &str,
     peer_id: PeerId,
 ) -> Result<String, anyhow::Error> {
+    let url = seed.join(project_id)?;
+
     git::git(
         repo,
         [
             "push",
-            "-q",
             "--signed",
             "--atomic",
-            &format!("{}/{}", seed, project_id),
+            url.as_str(),
             &format!(
                 "refs/namespaces/{}/refs/rad/ids/*:refs/remotes/{}/rad/ids/*",
                 project_id, peer_id
--- publish/src/main.rs
+++ publish/src/main.rs
@@ -6,9 +6,49 @@ use rad_common::{git, keys, profile, project, seed};
 use rad_terminal::compoments as term;
 
 use anyhow::Context as _;
+use url::Url;
+
+const GATEWAY_HOST: &str = "app.radicle.network";
+
+struct Options {
+    seed: Url,
+    verbose: bool,
+}
+
+impl Options {
+    fn from_env() -> anyhow::Result<Self> {
+        use lexopt::prelude::*;
+        use std::str::FromStr;
+
+        let mut parser = lexopt::Parser::from_env();
+        let mut seed: Url = Url::from_str("http://localhost:8778")?;
+        let mut verbose = false;
+
+        while let Some(arg) = parser.next()? {
+            match arg {
+                Long("seed") => {
+                    seed = parser
+                        .value()?
+                        .parse()
+                        .context("invalid URL specified for `--seed`")?;
+                }
+                Long("verbose") | Short('v') => {
+                    verbose = true;
+                }
+                _ => {
+                    return Err(anyhow::anyhow!(arg.unexpected()));
+                }
+            }
+        }
+
+        Ok(Options { seed, verbose })
+    }
+}
 
 fn main() -> anyhow::Result<()> {
-    match run() {
+    let options = Options::from_env()?;
+
+    match run(options) {
         Ok(()) => Ok(()),
         Err(err) => {
             term::format::error("Publishing failed", &err);
@@ -17,8 +57,8 @@ fn main() -> anyhow::Result<()> {
     }
 }
 
-fn run() -> anyhow::Result<()> {
-    let seed = "http://localhost:8778".to_string();
+fn run(options: Options) -> anyhow::Result<()> {
+    let seed = &options.seed;
     let profile = Profile::load()?;
     let (_, storage) = keys::storage(&profile, SshAuthSock::default())?;
 
@@ -26,14 +66,15 @@ fn run() -> anyhow::Result<()> {
 
     let repo = project::repository()?;
     let remote = project::remote(&repo)?;
+    let project_urn = &remote.url.urn;
     let project_id = Urn::encode_id(&remote.url.urn);
     let git_version = git::version()?;
 
     term::info(&format!(
         "Publishing 🌱 project {}",
-        term::format::highlight(&remote.url.urn.to_string())
+        term::format::highlight(project_urn)
     ));
-    term::info(&format!("Publishing to {}", term::format::highlight(&seed)));
+    term::info(&format!("Publishing to {}", term::format::highlight(seed)));
     term::info(&format!("Git version {}", git_version));
 
     if git_version < git::VERSION_REQUIRED {
@@ -44,26 +85,38 @@ fn run() -> anyhow::Result<()> {
     }
 
     let peer_id = profile::peer_id(&storage)?;
-    let urn = profile::user(&storage)?;
+    let user_urn = profile::user(&storage)?;
     let monorepo = profile.paths().git_dir();
-    let self_id = Urn::encode_id(&urn);
+    let self_id = Urn::encode_id(&user_urn);
     let signing_key = git::git(monorepo, ["config", "--local", git::CONFIG_SIGNING_KEY])
         .context("git signing key is not properly configured")?;
 
     term::info(&format!("Git signing key {}", signing_key));
 
     let mut spinner = term::spinner(&format!("Syncing delegate identity {}...", &self_id));
-    match seed::push_delegate_id(monorepo, &seed, &self_id, peer_id) {
-        Ok(_) => spinner.finish(),
+    match seed::push_delegate_id(monorepo, seed, &self_id, peer_id) {
+        Ok(output) => {
+            spinner.finish();
+
+            if options.verbose {
+                println!("{}", output);
+            }
+        }
         Err(err) => {
             spinner.failed();
             return Err(err);
         }
     }
 
-    spinner = term::spinner("Syncing project id...");
-    match seed::push_project_id(monorepo, &seed, &project_id, peer_id) {
-        Ok(_) => spinner.finish(),
+    spinner = term::spinner("Syncing project identity...");
+    match seed::push_project_id(monorepo, seed, &project_id, peer_id) {
+        Ok(output) => {
+            spinner.finish();
+
+            if options.verbose {
+                println!("{}", output);
+            }
+        }
         Err(err) => {
             spinner.failed();
             return Err(err);
@@ -71,14 +124,39 @@ fn run() -> anyhow::Result<()> {
     }
 
     spinner = term::spinner("Syncing project refs...");
-    match seed::push_refs(monorepo, &seed, &project_id, peer_id) {
-        Ok(_) => spinner.finish(),
+    match seed::push_refs(monorepo, seed, &project_id, peer_id) {
+        Ok(output) => {
+            spinner.finish();
+
+            if options.verbose {
+                println!("{}", output);
+            }
+        }
         Err(err) => {
             spinner.failed();
             return Err(err);
         }
     }
-    term::success("Project published.");
+    term::success("Project published");
+    term::blank();
+
+    if let Some(host) = seed.host() {
+        let is_routable = match host {
+            url::Host::Domain("localhost") => false,
+            url::Host::Domain(_) => true,
+            url::Host::Ipv4(ip) => !ip.is_loopback() && !ip.is_unspecified() && !ip.is_private(),
+            url::Host::Ipv6(ip) => !ip.is_loopback() && !ip.is_unspecified(),
+        };
+        let git_url = seed.join(&project_id)?;
+
+        if is_routable {
+            term::info(&format!(
+                "Your project is available at https://{}/seeds/{}/projects/{} (Web)",
+                GATEWAY_HOST, host, project_urn,
+            ));
+        }
+        term::info(&format!("Your project is available at {} (Git)", git_url));
+    }
 
     Ok(())
 }
