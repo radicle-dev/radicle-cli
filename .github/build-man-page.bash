3a0345be9b0dee387ac795a2aceeb26909e7ca4b
Adam Szkoda <adaszko@gmail.com>
2022-02-11 12:30:04 +0100

    Build all man pages automatically

--- .github/build-man-page.bash
+++ .github/build-man-page.bash
@@ -3,14 +3,20 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -o errtrace
+export SHELLOPTS
 
 main () {
-    if [[ $# -gt 1 ]]; then
-        echo "Expected an .adoc file as the sole argument"
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: $0 FILE.adoc..."
         return 1
     fi
-    local adoc_file_path="$1"; shift
-    asciidoctor --doctype manpage --backend manpage --out-file - "$adoc_file_path" | gzip --best --stdout
+
+    while [[ $# -gt 0 ]]; do
+        local adoc_file_path="$1"; shift
+        local output_file_path="$(basename "$adoc_file_path" .adoc).gz"
+        asciidoctor --doctype manpage --backend manpage --out-file - "$adoc_file_path" | gzip --best --stdout >"$output_file_path"
+    done
 }
 
 main "$@"
