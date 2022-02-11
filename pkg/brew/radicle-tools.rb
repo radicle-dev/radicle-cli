77758a10f68fcc6299330c7323462755d1bf816f
Adam Szkoda <adaszko@gmail.com>
2022-02-11 09:56:06 +0100

    Add Homebrew formula

---
+++ pkg/brew/radicle-tools.rb
@@ -0,0 +1,31 @@
+class RadicleTools < Formula
+  version "0.0.0"
+  desc "Radicle CLI tools"
+  homepage "https://app.radicle.network/alt-clients.radicle.eth/radicle-client-tools"
+
+  url "https://github.com/radicle-dev/radicle-client-tools/releases/download/v#{version}/radicle-tools-x86_64-apple-darwin.tar.gz"
+  sha256 "30dc8a5627f41b0ab4b71fb7260b01167cba1c0f25dd875829ee0ebdc83dc7dd"
+
+
+  def install
+    bin.install "rad-show"
+    bin.install "rad-account"
+    bin.install "rad-sync"
+    bin.install "rad-help"
+    bin.install "rad-ens"
+    bin.install "rad-push"
+    bin.install "rad"
+    bin.install "rad-auth"
+    bin.install "rad-track"
+    bin.install "rad-ls"
+    bin.install "rad-init"
+    bin.install "rad-checkout"
+    bin.install "rad-untrack"
+    bin.install "git-remote-rad"
+    man1.install "radicle-tools.1.gz"
+  end
+
+  test do
+    system "rad"
+  end
+end
