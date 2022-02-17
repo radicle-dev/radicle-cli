954578e88d28d07c44f68e0905401b943cf32dd6
xphoniex <dj.2dixx@gmail.com>
2022-02-17 11:15:28 +0000

    rad-show: Move `run` fn to lib.rs
    Signed-off-by: xphoniex <dj.2dixx@gmail.com>

--- show/src/lib.rs
+++ show/src/lib.rs
@@ -1,6 +1,8 @@
 use std::ffi::OsString;
 
+use rad_common::{keys, person, profile, project};
 use rad_terminal::args::{Args, Error, Help};
+use rad_terminal::components as term;
 
 pub const HELP: Help = Help {
     name: "show",
@@ -77,4 +79,42 @@ impl Args for Options {
             vec![],
         ))
     }
+}
+
+pub fn run(mut options: Options) -> anyhow::Result<()> {
+    let profile = profile::default()?;
+    let sock = keys::ssh_auth_sock();
+    let (_, storage) = keys::storage(&profile, sock)?;
+
+    if options == Options::default() {
+        options.show_proj_id = true;
+        options.show_peer_id = true;
+        options.show_self = true;
+        options.show_profile_id = true;
+        options.show_ssh_key = true;
+    }
+
+    if options.show_proj_id {
+        let repo = project::repository()?;
+        let remote = project::remote(&repo)?;
+        let urn = remote.url.urn;
+
+        term::info!("project: {}", term::format::highlight(urn));
+    }
+    if options.show_peer_id {
+        term::info!("peer: {}", term::format::highlight(storage.peer_id()));
+    }
+    if options.show_self {
+        let id = person::local(&storage)?;
+        term::info!("self: {}", term::format::highlight(id.urn()));
+    }
+    if options.show_profile_id {
+        term::info!("profile: {}", term::format::highlight(profile.id()));
+    }
+    if options.show_ssh_key {
+        let peer_id = storage.peer_id();
+        let ssh = keys::to_ssh_fingerprint(peer_id)?;
+        term::info!("ssh: {}", term::format::highlight(ssh));
+    }
+    Ok(())
 }
