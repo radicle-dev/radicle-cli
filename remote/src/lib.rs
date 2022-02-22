c16a6f577688ee43a576d3ffe56fc02a8e84f7eb
Alexis Sellier <self@cloudhead.io>
2022-02-22 18:01:48 +0100

    rad-remote: Deal better with duplicates
    Signed-off-by: Alexis Sellier <self@cloudhead.io>

--- remote/src/lib.rs
+++ remote/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::ffi::OsString;
 
 use anyhow::anyhow;
@@ -153,11 +154,9 @@ pub fn run(options: Options) -> anyhow::Result<()> {
                         repo.remote_delete(&name)?;
                         term::success!(
                             "Remote {} {} removed",
-                            term::format::highlight(remote),
+                            term::format::highlight(&remote),
                             term::format::dim(format!("{:?}", name)),
                         );
-
-                        break;
                     }
                 }
             } else {
@@ -168,11 +167,9 @@ pub fn run(options: Options) -> anyhow::Result<()> {
                             repo.remote_delete(&name)?;
                             term::success!(
                                 "Remote {} {} removed",
-                                term::format::highlight(remote),
+                                term::format::highlight(&remote),
                                 term::format::dim(format!("{:?}", name)),
                             );
-
-                            break;
                         }
                     }
                 }
@@ -182,8 +179,14 @@ pub fn run(options: Options) -> anyhow::Result<()> {
             let mut table = term::Table::default();
             let proj = project::get(&storage, &urn)?
                 .ok_or_else(|| anyhow!("project {} not found on local device", urn))?;
+            let mut peers = HashSet::new();
 
             for (_, peer) in git::remotes(&repo)? {
+                if !peers.insert(peer) {
+                    // Don't show duplicate peers.
+                    continue;
+                }
+
                 let delegate = if proj.remotes.contains(&peer) {
                     term::format::badge_primary("delegate")
                 } else {
