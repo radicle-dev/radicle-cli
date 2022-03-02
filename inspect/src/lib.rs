cee820337320ab5b9ab3b7e72f17e4ad59cb0ce8
Alexis Sellier <self@cloudhead.io>
2022-03-02 23:31:17 +0100

    rad-inspect: Add `--refs` and `--payload`
    Signed-off-by: Alexis Sellier <self@cloudhead.io>

--- inspect/src/lib.rs
+++ inspect/src/lib.rs
@@ -1,5 +1,6 @@
 use std::ffi::OsString;
 use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
 use std::str::FromStr;
 
 use rad_common::{git, profile, project};
@@ -25,11 +26,13 @@ Usage
     rad inspect
 
     Inspects the given path or URN. If neither is specified,
-    the current directory is inspected.
+    the current project is inspected.
 
 Options
 
-    --help   Print help
+    --payload   Inspect the object's payload
+    --refs      Inspect the object's refs on the local device (requires `tree`)
+    --help      Print help
 "#,
 };
 
@@ -37,6 +40,8 @@ Options
 pub struct Options {
     pub path: Option<PathBuf>,
     pub urn: Option<Urn>,
+    pub refs: bool,
+    pub payload: bool,
 }
 
 impl Args for Options {
@@ -46,11 +51,19 @@ impl Args for Options {
         let mut parser = lexopt::Parser::from_args(args);
         let mut path: Option<PathBuf> = None;
         let mut urn: Option<Urn> = None;
+        let mut refs = false;
+        let mut payload = false;
 
         while let Some(arg) = parser.next()? {
             match arg {
                 Long("help") => {
                     return Err(Error::Help.into());
+                }
+                Long("refs") => {
+                    refs = true;
+                }
+                Long("payload") => {
+                    payload = true;
                 }
                 Value(val) if path.is_none() && urn.is_none() => {
                     let val = val.to_string_lossy();
@@ -69,7 +82,15 @@ impl Args for Options {
             }
         }
 
-        Ok((Options { path, urn }, vec![]))
+        Ok((
+            Options {
+                path,
+                payload,
+                refs,
+                urn,
+            },
+            vec![],
+        ))
     }
 }
 
@@ -77,7 +98,26 @@ pub fn run(options: Options) -> anyhow::Result<()> {
     let profile = profile::default()?;
     let storage = profile::read_only(&profile)?;
 
-    if let Some(urn) = options.urn {
+    let urn = if let Some(urn) = options.urn {
+        urn
+    } else {
+        let repo =
+            git::Repository::open(options.path.unwrap_or_else(|| Path::new(".").to_path_buf()))?;
+
+        project::remote(&repo)?.url.urn
+    };
+
+    if options.refs {
+        let path = profile.paths().git_dir().join("refs").join("namespaces");
+
+        Command::new("tree")
+            .current_dir(path)
+            .args([&urn.encode_id(), "--noreport", "--prune"])
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()?
+            .wait()?;
+    } else if options.payload {
         let payload = any::get(&storage, &urn)
             .map(|o| o.map(|p| p.payload()))
             .map_err(|_| anyhow::anyhow!("Couldn't load project or person."))?
@@ -88,10 +128,6 @@ pub fn run(options: Options) -> anyhow::Result<()> {
             serde_json::to_string_pretty(&payload)?.to_colored_json_auto()?
         );
     } else {
-        let repo =
-            git::Repository::open(options.path.unwrap_or_else(|| Path::new(".").to_path_buf()))?;
-        let urn = project::remote(&repo)?.url.urn;
-
         term::info!("{}", term::format::highlight(urn));
     }
 
