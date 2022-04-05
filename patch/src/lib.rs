4a822a1d66ec890db3f7f9a68015668760d68013
erikli <erik@zirkular.io>
2022-04-05 13:29:50 +0200

    rad-patch: Evaluates CLI options

--- patch/src/lib.rs
+++ patch/src/lib.rs
@@ -1,11 +1,11 @@
 use std::ffi::OsString;
 
-pub use git2::{Oid, Reference};
-
-pub use lnk_identities::refs;
+use anyhow::anyhow;
 
 use rad_terminal::args::{Args, Error, Help};
 use rad_terminal::components as term;
+
+use rad_common::{keys, profile, project};
 
 pub const HELP: Help = Help {
     name: "patch",
@@ -13,9 +13,9 @@ pub const HELP: Help = Help {
     version: env!("CARGO_PKG_VERSION"),
     usage: r#"
 Usage
+
     rad patch [<option>...]
 
-    Creates a new patch.
 Options
     --list    Prints all patches (default: false)
     --help    Print help
@@ -55,7 +55,32 @@ impl Args for Options {
     }
 }
 
-pub fn run(_options: Options) -> anyhow::Result<()> {
+pub fn run(options: Options) -> anyhow::Result<()> {
+    let (urn, _repo) = project::cwd()
+        .map_err(|_| anyhow!("this command must be run in the context of a project"))?;
+
+    let profile = profile::default()?;
+    let sock = keys::ssh_auth_sock();
+    let (_signer, storage) = keys::storage(&profile, sock)?;
+    let _project = project::get(&storage, &urn)?
+        .ok_or_else(|| anyhow!("couldn't load project {} from local state", urn))?;
+
+    if options.list {
+        list()?;
+    } else {
+        create()?;
+    }
+
+    Ok(())
+}
+
+fn list() -> anyhow::Result<()> {
+    term::warning("Not implemented yet!");
+
+    Ok(())
+}
+
+fn create() -> anyhow::Result<()> {
     term::warning("Not implemented yet!");
 
     Ok(())
