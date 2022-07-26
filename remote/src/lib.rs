7df8318dc626f24c35581e58a2ec837d58ffc464
Slack Coder <slackcoder@server.ky>
2022-07-26 09:48:40 -0500

    remote: Move list fn to termninal crate
    Consoildate the code by following the same pattern as other commands.

--- remote/src/lib.rs
+++ remote/src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::ffi::OsString;
 
 use anyhow::anyhow;
@@ -144,36 +143,7 @@ pub fn run(options: Options, ctx: impl term::Context) -> anyhow::Result<()> {
             );
         }
         Operation::Remove { remote } => term::remote::remove(&remote, &storage, &repo, &urn)?,
-        Operation::List => {
-            let mut table = term::Table::default();
-            let proj = project::get(&storage, &urn)?
-                .ok_or_else(|| anyhow!("project {} not found on local device", urn))?;
-            let mut peers = HashSet::new();
-
-            for (_, peer) in git::remotes(&repo)? {
-                if !peers.insert(peer) {
-                    // Don't show duplicate peers.
-                    continue;
-                }
-
-                let delegate = if proj.remotes.contains(&peer) {
-                    term::format::badge_primary("delegate")
-                } else {
-                    String::new()
-                };
-
-                if let Some(person) = project::person(&storage, urn.clone(), &peer)? {
-                    table.push([
-                        term::format::bold(person.subject().name.to_string()),
-                        term::format::tertiary(peer),
-                        delegate,
-                    ]);
-                } else {
-                    table.push([String::new(), term::format::tertiary(peer), delegate]);
-                }
-            }
-            table.render();
-        }
+        Operation::List => term::remote::list(&storage, &repo, &urn)?,
     }
 
     Ok(())
